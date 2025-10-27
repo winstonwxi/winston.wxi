7e7dfc2ec796cd5ebc0fee7e0f9b1fe3776f0580
winstonwxi <winstonjr2007@gmail.com>
2025-10-26 23:40:35 -0400

    made some bullshit

---
+++ my-website/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+    <h1>Welcome to my website</h1>
+    <p>This is my first React page.</p>
+  </div>
+  );
+}
+
+export default App;
